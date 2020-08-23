1718c1c9121189995af56a81b4fdd4d5f8976148
Tim Paler <trpaler@up.edu.ph>
2020-08-24 03:09:33 +0800

    Display table and csv uploaded data

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,7 +1,8 @@
+import json
 from django.shortcuts import render
 
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.urls import reverse
 
 from weatherapp.models import Document
@@ -10,25 +11,24 @@ from weatherapp.forms import DocumentForm
 def index(request):
     # Handle file upload
     context = {}
+    lines = []
     if request.method == 'POST':
-        print(request.POST)
-        print('asdasdasdasdasd')
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             docfile = request.FILES['docfile']
             for line in docfile:
-                print(line)
+                lines.append(str(line))
+
             newdoc = Document(docfile=docfile, date=request.POST.get('date'))
             newdoc.save()
-            return HttpResponseRedirect(reverse('index'))
-        else:
-            print(form.errors)
+            context['lines'] = str(lines)
+            context['status'] = 200
+
     else:
         form = DocumentForm()
 
     # Load documents for the index page
     documents = Document.objects.all()
-    context = {'documents': documents, 'form': form}
-
+    context = {'documents': documents, 'form': form, 'lines': lines}
     # Render index page with the documents and the form
     return render(request, 'weatherapp/index.html', context)
