2cfd982c5b0efb5c8ea0d3482d48a2e2f1942936
Tim Paler <trpaler@up.edu.ph>
2020-08-26 00:53:11 +0800

    Update typo

--- codingproject/settings.py
+++ codingproject/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = (
 
 if 'DATABASE_URL' in os.environ:
     import dj_database_url
-    DATABASES = {'default': dj_database_url.config()
+    DATABASES = {'default': dj_database_url.config()}
